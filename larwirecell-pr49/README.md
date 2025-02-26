ee25d425c288fe6b9d01b2fe370f63ea54645125
HaiwangYu <yuhw.pku@gmail.com>
2025-02-26 15:25:03 -0600

    init

---
+++ larwirecell-pr49/README.md
@@ -0,0 +1,5 @@
+
+
+```bash!
+lar -n 1 -c pdsp.fcl -s xroot://fndca1.fnal.gov:1095/pnfs/fnal.gov/usr/dune/persistent/stash/ContinuousIntegration/protoDUNEsp/datareco/np04_raw_run005809_0004_dl6.root -o tmp.root
+```
---
+++ larwirecell-pr49/setup.sh
@@ -0,0 +1,5 @@
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup dunesw v10_02_02d00 -q e26:prof
+source /exp/dune/app/users/yuhw/larsoft/v10_02_02d00/localProducts_larsoft_v10_02_02_e26_prof/setup
+mrbsetenv
+mrbslp
