a6512f2aa4422ed11c1b6b5d002c3cabe483a2d3
HaiwangYu <yuhw.pku@gmail.com>
2025-04-22 07:54:32 -0700

    save some dev on wcgpu

--- larwirecell-pr49/README.md
+++ larwirecell-pr49/README.md
@@ -2,4 +2,5 @@
 
 ```bash!
 lar -n 1 -c pdsp.fcl -s xroot://fndca1.fnal.gov:1095/pnfs/fnal.gov/usr/dune/persistent/stash/ContinuousIntegration/protoDUNEsp/datareco/np04_raw_run005809_0004_dl6.root -o tmp.root
+lar -n 1 -c pdsp.fcl -s np04_raw_run005809_0004_dl6.root -o tmp.root
 ```
--- larwirecell-pr49/setup.sh
+++ larwirecell-pr49/setup.sh
@@ -1,5 +1,12 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 setup dunesw v10_02_02d00 -q e26:prof
-source /exp/dune/app/users/yuhw/larsoft/v10_02_02d00/localProducts_larsoft_v10_02_02_e26_prof/setup
-mrbsetenv
-mrbslp
+
+# gpvm
+# source /exp/dune/app/users/yuhw/larsoft/v10_02_02d00/localProducts_larsoft_v10_02_02_e26_prof/setup
+# mrbsetenv
+# mrbslp
+
+# wcgpu
+# path-prepend /nfs/data/1/yuhw/opt/lib LD_LIBRARY_PATH
+# WCT_SRC=/home/yuhw/nfs/wire-cell-toolkit
+# path-prepend ${WCT_SRC}/cfg/ WIRECELL_PATH
