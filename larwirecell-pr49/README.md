625a80075cec51a8c6c1f54d5817d5e8a752e502
HaiwangYu <yuhw.pku@gmail.com>
2025-08-13 14:37:15 -0500

    catch up

--- larwirecell-pr49/README.md
+++ larwirecell-pr49/README.md
@@ -1,5 +1,13 @@
 
 
 ```bash!
+./run.sh
+```
+This seems to take long:
+```bash!
+Attempting to get token from https://htvaultprod.fnal.gov:8200 .../exp/dune/app/users/yuhw/wct-ci/larwirecell-pr49
+```
+
+```bash!
 lar -n 1 -c pdsp.fcl -s xroot://fndca1.fnal.gov:1095/pnfs/fnal.gov/usr/dune/persistent/stash/ContinuousIntegration/protoDUNEsp/datareco/np04_raw_run005809_0004_dl6.root -o tmp.root
 ```
--- larwirecell-pr49/setup.sh
+++ larwirecell-pr49/setup.sh
@@ -1,5 +1,13 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
-setup dunesw v10_02_02d00 -q e26:prof
-source /exp/dune/app/users/yuhw/larsoft/v10_02_02d00/localProducts_larsoft_v10_02_02_e26_prof/setup
+
+# setup dunesw v10_02_02d00 -q e26:prof
+# source /exp/dune/app/users/yuhw/larsoft/v10_02_02d00/localProducts_larsoft_v10_02_02_e26_prof/setup
+
+setup dunesw v10_04_03d01 -q e26:prof
+source /exp/dune/app/users/yuhw/larsoft/v10_04_03d01/localProducts_larsoft_v10_04_03_e26_prof/setup
+
 mrbsetenv
 mrbslp
+
+path-prepend /exp/dune/app/users/yuhw/opt/lib64 LD_LIBRARY_PATH
+path-prepend /exp/dune/app/users/yuhw/wct/cfg WIRECELL_PATH
