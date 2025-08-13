49fb673b0e3b67031c9546c495e2688da6a431a6
HaiwangYu <yuhw.pku@gmail.com>
2025-08-13 14:38:19 -0500

    iceberg

---
+++ simchannel/README.md
@@ -0,0 +1,4 @@
+
+```bash
+export WIRECELL_PATH=/exp/dune/app/users/yuhw/wct-ci/simchannel/cfg:$WIRECELL_PATH
+```
---
+++ simchannel/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ~/.bash_profile
+
+source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+setup dunesw v10_05_00d00 -q e26:prof
